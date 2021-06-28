75f9f55c80ef41c3b2206c6f0a6db96ec37c2e60
Levi Matus <thelevimatusproject@gmail.com>
2021-06-28 11:54:27 -0300

    add high-level driver that is composed of all drivers

--- service/internal/driver/driver.go
+++ service/internal/driver/driver.go
@@ -1,11 +1,14 @@
 package driver
 
 import (
+	"context"
+
 	"github.com/LeviMatus/readcommend/service/internal/driver/author"
 	"github.com/LeviMatus/readcommend/service/internal/driver/book"
 	"github.com/LeviMatus/readcommend/service/internal/driver/era"
 	"github.com/LeviMatus/readcommend/service/internal/driver/genre"
 	"github.com/LeviMatus/readcommend/service/internal/driver/size"
+	"github.com/LeviMatus/readcommend/service/internal/entity"
 	"github.com/pkg/errors"
 )
 
@@ -37,4 +40,24 @@ func New(a author.Driver, g genre.Driver, s size.Driver, e era.Driver, b book.Dr
 		eraDriver:    e,
 		bookDriver:   b,
 	}, nil
+}
+
+func (d driver) ListAuthors(ctx context.Context) ([]entity.Author, error) {
+	return d.authorDriver.ListAuthors(ctx)
+}
+
+func (d driver) ListGenres(ctx context.Context) ([]entity.Genre, error) {
+	return d.genreDriver.ListGenres(ctx)
+}
+
+func (d driver) ListSizes(ctx context.Context) ([]entity.Size, error) {
+	return d.sizeDriver.ListSizes(ctx)
+}
+
+func (d driver) ListEras(ctx context.Context) ([]entity.Era, error) {
+	return d.eraDriver.ListEras(ctx)
+}
+
+func (d driver) SearchBooks(ctx context.Context, params book.SearchInput) ([]entity.Book, error) {
+	return d.bookDriver.SearchBooks(ctx, params)
 }
