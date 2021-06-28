70c596cc2fd4a760f7b42685d3b3f7dfd287b9aa
Levi Matus <thelevimatusproject@gmail.com>
2021-06-28 11:53:25 -0300

    remove domain models in lieu of entity models

--- service/internal/domain/Genre.go
+++
@@ -1,7 +0,0 @@
-package domain
-
-type Genre struct {
-	// TODO: find appropriate int category
-	ID    int    `json:"id"`
-	Title string `json:"title"`
-}
--- service/internal/domain/author.go
+++
@@ -1,8 +0,0 @@
-package domain
-
-type Author struct {
-	// TODO: find appropriate int category
-	ID        int    `json:"id"`
-	FirstName string `json:"firstName"`
-	LastName  string `json:"lastName"`
-}
