6232f2370af09e45ed4bfe51dfdaf730657fc038
Levi Matus <thelevimatusproject@gmail.com>
2021-06-28 11:49:05 -0300

    add api and db configs

--- service/pkg/config/config.go
+++ service/pkg/config/config.go
@@ -1,23 +1,27 @@
 package config
 
+import (
+	"time"
+)
+
 type Config struct {
 	Database
 	API
 }
 
 type Database struct {
-	Host     string
-	Port     string
-	Database string
-	Schema   string
-	SSL      string
-	Username string
-	Password string
+	Host         string
+	Port         string
+	Database     string
+	Schema       string
+	SSL          string
+	Username     string
+	Password     string
+	DefaultLimit uint64
 }
 
 type API struct {
-	Port         string
-	Interface    string
-	DefaultLimit uint64
-	Timeout      int16
+	Port      string
+	Interface string
+	Timeout   time.Duration
 }
