2d067fc4c03a53a8b44d27aeaa2dafb3d0a80f05
Levi Matus <thelevimatusproject@gmail.com>
2021-06-28 11:55:41 -0300

    add executable for the api

--- service/cmd/api/main.go
+++ service/cmd/api/main.go
@@ -1,11 +1,21 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
+	"time"
 
 	"github.com/LeviMatus/readcommend/service/internal/api"
-	"github.com/LeviMatus/readcommend/service/internal/repository/driver"
+	"github.com/LeviMatus/readcommend/service/internal/driver"
+	"github.com/LeviMatus/readcommend/service/internal/driver/author"
+	"github.com/LeviMatus/readcommend/service/internal/driver/book"
+	"github.com/LeviMatus/readcommend/service/internal/driver/era"
+	"github.com/LeviMatus/readcommend/service/internal/driver/genre"
+	"github.com/LeviMatus/readcommend/service/internal/driver/size"
+	"github.com/LeviMatus/readcommend/service/internal/infra/repository/postgres"
 	"github.com/LeviMatus/readcommend/service/pkg/config"
+	_ "github.com/lib/pq"
 )
 
 func main() {
@@ -23,15 +33,60 @@ func main() {
 		API: config.API{
 			Port:      "5000",
 			Interface: "0.0.0.0",
+			Timeout:   15 * time.Second,
 		},
 	}
 
-	dbDriver, err := driver.New(cfg.Database)
+	conStr := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		cfg.Database.Host, cfg.Database.Port, cfg.Database.Username, cfg.Database.Password, cfg.Database.Database, cfg.Database.SSL)
+
+	if cfg.Database.Schema != "" {
+		conStr = fmt.Sprintf("%s search_path=%s", conStr, cfg.Database.Schema)
+	}
+
+	db, err := sql.Open("postgres", conStr)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	r, err := api.New(dbDriver.Driver(), cfg.API)
+	if err := db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+
+	bookRepo, err := postgres.NewBookRepository(db)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	authorRepo, err := postgres.NewAuthorRepository(db)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	genreRepo, err := postgres.NewGenreRepository(db)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	eraRepo, err := postgres.NewEraRepository(db)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sizeRepo, err := postgres.NewSizeRepository(db)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	d, err := driver.New(
+		author.NewDriver(authorRepo),
+		genre.NewDriver(genreRepo),
+		size.NewDriver(sizeRepo),
+		era.NewDriver(eraRepo),
+		book.NewDriver(bookRepo))
+
+	r, err := api.New(d, cfg.API)
 	if err != nil {
 		log.Fatal(err)
 	}
