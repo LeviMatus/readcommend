392dc593eb4c61fb786101547c75676f1e5d73ad
Levi Matus <thelevimatusproject@gmail.com>
2021-06-27 23:29:27 -0300

    add genre driver

---
+++ service/internal/driver/genre/driver.go
@@ -0,0 +1,19 @@
+package genre
+
+import (
+	"context"
+
+	"github.com/LeviMatus/readcommend/service/internal/entity"
+)
+
+type driver struct {
+	repository Repository
+}
+
+func NewDriver(r Repository) *driver {
+	return &driver{repository: r}
+}
+
+func (d *driver) List(ctx context.Context) ([]entity.Genre, error) {
+	return d.repository.List(ctx)
+}
