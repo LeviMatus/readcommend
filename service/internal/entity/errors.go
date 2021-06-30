5b291176c594fd34180f0f037a804ab8ca7afa7e
Levi Matus <thelevimatusproject@gmail.com>
2021-06-30 17:24:54 -0300

    add comments to public errors

--- service/internal/entity/errors.go
+++ service/internal/entity/errors.go
@@ -5,5 +5,6 @@ import (
 )
 
 var (
+	// ErrInvalidQueryParam occurs when an invalid parameter range or type was provided.
 	ErrInvalidQueryParam = errors.New("invalid URL query parameter provided")
 )
