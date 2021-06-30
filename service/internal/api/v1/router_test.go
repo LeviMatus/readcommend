1f84f1b4dbe3d9099331acf468eebdec515a1db0
Levi Matus <thelevimatusproject@gmail.com>
2021-06-30 11:44:55 -0300

    remove unused test function

--- service/internal/api/v1/router_test.go
+++ service/internal/api/v1/router_test.go
@@ -1,9 +1,6 @@
 package v1
 
 import (
-	"fmt"
-	"net/http"
-	"net/http/httptest"
 	"testing"
 
 	"github.com/LeviMatus/readcommend/service/internal/driver"
@@ -33,33 +30,6 @@ func TestNewRouter(t *testing.T) {
 			r, err := NewRouter(tt.driver)
 			tt.errAssertion(t, err)
 			tt.valAssertion(t, r)
-		})
-	}
-}
-
-func TestGetBooks(t *testing.T) {
-	driverMock := drivertest.DriverMock{}
-
-	tests := map[string]struct {
-		target                string
-		method                string
-		expectedCode          int
-		sendRequest           func(url string) (*http.Response, error)
-		setDriverExpectations func(d *drivertest.DriverMock)
-	}{}
-
-	for name, tt := range tests {
-		t.Run(name, func(t *testing.T) {
-			tt.setDriverExpectations(&driverMock)
-			router, err := NewRouter(&driverMock)
-			assert.NoError(t, err)
-
-			testServer := httptest.NewServer(router)
-			defer testServer.Close()
-
-			resp, err := tt.sendRequest(fmt.Sprintf("%s%s", testServer.URL, tt.target))
-			assert.NoError(t, err)
-			assert.Equal(t, tt.expectedCode, resp.StatusCode)
 		})
 	}
 }
